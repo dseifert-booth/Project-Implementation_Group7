ddbe16b44eb08d14da1c410db4dcafc4068ead47
Darius <dariusasb@gmail.com>
2023-02-16 21:36:10 -0500

    Added Data Tables: RoleClaim, Shift, Task

---
+++ PRJ666_G7-Project/Data/RoleClaim.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace PRJ666_G7_Project.Data
+{
+    public class RoleClaim
+    {
+        public int Id { get; set; }
+
+        [Required, StringLength(100)]
+        public string Name { get; set; }
+    }
+}
