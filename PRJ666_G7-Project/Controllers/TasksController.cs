ec100537c4820de2e17ad459b8e33fb05b28ce4d
Darius <dariusasb@gmail.com>
2023-02-17 21:10:04 -0500

    created controllers + template views for Shifts / Tasks

---
+++ PRJ666_G7-Project/Controllers/TasksController.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using PRJ666_G7_Project.Data;
+using PRJ666_G7_Project.Models;
+
+namespace PRJ666_G7_Project.Controllers
+{
+    public class TasksController : Controller
+    {
+        private ApplicationDbContext db = new ApplicationDbContext();
+
+        // GET: Tasks
+        public ActionResult Index()
+        {
+            return View(db.Tasks.ToList());
+        }
+
+        // GET: Tasks/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Task task = db.Tasks.Find(id);
+            if (task == null)
+            {
+                return HttpNotFound();
+            }
+            return View(task);
+        }
+
+        // GET: Tasks/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Tasks/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,Name,Description,Complete")] Task task)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Tasks.Add(task);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(task);
+        }
+
+        // GET: Tasks/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Task task = db.Tasks.Find(id);
+            if (task == null)
+            {
+                return HttpNotFound();
+            }
+            return View(task);
+        }
+
+        // POST: Tasks/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,Name,Description,Complete")] Task task)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(task).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(task);
+        }
+
+        // GET: Tasks/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Task task = db.Tasks.Find(id);
+            if (task == null)
+            {
+                return HttpNotFound();
+            }
+            return View(task);
+        }
+
+        // POST: Tasks/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Task task = db.Tasks.Find(id);
+            db.Tasks.Remove(task);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ PRJ666_G7-Project/Models/ShiftBaseViewModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace PRJ666_G7_Project.Models
+{
+    public class ShiftBaseViewModel
+    {
+
+        [Key]
+        public int Id { get; set; }
+
+        [Display(Name = "Start time")]
+        public DateTime ShiftStart { get; set; }
+
+        [Display(Name = "End time")]
+        public DateTime ShiftEnd { get; set; }
+
+        [Display(Name = "Assigned employee")]
+        [StringLength(200)]
+        public string Employee { get; set; }
+
+    }
+}
